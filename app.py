e7dc94fec535f6d6d62a58309d539269d54d9fa7
siwarkhateb20-meet <40965544+siwarkhateb20-meet@users.noreply.github.com>
2020-01-26 20:38:49 +0200

    my files

--- app.py
+++ app.py
@@ -6,12 +6,39 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 from database import *	
 
-##### Code here ######
 @app.route('/')
 def homepage():
-	return render_template("home.html")
+	return render_template("homepage.html")
 
-#####################
+@app.route('/about')
+def aboutpage():
+	return render_template("about.html")
+
+@app.route('/login')
+def loginpage():
+	return render_template("login.html")
+
+@app.route('/signin')
+def signinpage():
+	return render_template("signin.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = get_user(request.form['username'])
+    if user != None and user.verify_password(request.form["password_hash"]):
+        login_session['name'] = user.username
+        return home()
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    #check that username isn't already taken
+    user = get_user(request.form['username'])
+    if user == None:
+        add_user(request.form['username'],request.form['password'], request.form['gender'])
+    return home()
+
+
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,6 +1,36 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_security
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    #what else does a user account need?
+    password_hash = Column(String)
+    username = Column(String)
+    gender = Column(String)
+
+
+
+
+    def hash_password(self, password):
+        self.password_hash = pwd_security.encrypt(password)
+    def verify_password(self, password):
+        return pwd_security.verify(password, self.password_hash)
+
+"""
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    #what else does a user account need?
+    password_hash = Column(String)
+    name = Column(String)
+    gender = Column(String)
+"""
+
 
